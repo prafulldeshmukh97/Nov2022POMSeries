fc5e63a4c416605ff8cefbff8a9fc852efc51267
Prafull Deshmukh <prafull.deshmukh97@gmail.com>
2024-12-08 20:56:37 +0530

    Updated changes as per PR review comments

--- src/main/java/com/qa/opencart/pages/OrderPage.java
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -1,13 +1,16 @@
 package com.qa.opencart.pages;
-
 import org.openqa.selenium.By;
 
 public class OrderPage {
 
 	By loc =By.id("test");
 	
-	public void add()
+	public void getOrder()
 	{
-		
+		System.out.println("getOrder");
+	}
+	public void getPrice()
+	{
+		System.out.println("price");
 	}
 }
